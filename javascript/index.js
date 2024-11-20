d745049a1e7ec7b49464bb873734fcd32e303054
Eugene2405 <galuskozena@gmail.com>
2024-11-20 21:08:35 +0200

    added sidebar, fixed positioning

--- javascript/index.js
+++ javascript/index.js
@@ -12,5 +12,55 @@ function updateRotation() {
     ".guitar"
   ).style.transform = `rotateZ(${-rotation}deg)`;
 }
+
+const menuCheckbox = document.getElementById("menu-checkbox");
+const navMenuButton = document.querySelector(".nav-menu-btn");
+const sidebar = document.querySelector(".sidebar");
+const navHeight = document.querySelector(".navbar").offsetHeight;
+
+function checkboxState() {
+  if (menuCheckbox.checked) {
+    navMenuButton.style.transform = "rotate(90deg)";
+    sidebar.style.left = "0";
+  } else {
+    navMenuButton.style.transform = "rotate(0)";
+    sidebar.style.left = "-20rem";
+    sidebar.style.top = `${navHeight - 2}px`;
+  }
+}
+
+const toggleButtons = document.querySelectorAll(".toggle-button");
+
+toggleButtons.forEach((button) => {
+  const dropdown = button.nextElementSibling;
+  const dropdownScrollHeight = dropdown.scrollHeight + 2;
+
+  button.addEventListener("click", () => {
+    console.log(dropdownScrollHeight);
+    dropdown.classList.toggle("shown");
+
+    console.log(dropdown.offsetHeight);
+    if (!dropdown.classList.contains("shown")) {
+      dropdown.style.height = "0";
+    } else {
+      dropdown.style.height = `${dropdownScrollHeight}px`;
+    }
+
+    button.classList.toggle("clicked");
+
+    toggleButtons.forEach((otherButton) => {
+      if (otherButton != button) {
+        otherButton.classList.remove("clicked");
+        otherButton.nextElementSibling.classList.remove("shown");
+        otherButton.nextElementSibling.style.height = "0";
+      }
+    });
+  });
+});
+
+menuCheckbox.addEventListener("change", checkboxState);
+window.addEventListener("load", () => {
+  sidebar.style.top = `${navHeight - 2}px`;
+});
 window.addEventListener("resize", updateRotation);
 window.addEventListener("load", updateRotation);
